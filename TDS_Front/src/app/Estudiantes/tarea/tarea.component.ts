c58afd0b0cf611288f91bea33ae2bfb99d1fa3c8
gp400 <79920471+gp400@users.noreply.github.com>
2022-12-06 00:18:45 -0400

    Add files via upload

--- TDS_Front/src/app/Estudiantes/tarea/tarea.component.ts
+++ TDS_Front/src/app/Estudiantes/tarea/tarea.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Entrega } from 'src/app/models/entrega';
 import { Tarea } from 'src/app/models/tarea';
 import { Usuario } from 'src/app/models/usuario';
 import { APIService } from 'src/app/services/api.service';
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../../../firebase/provider";
+import Swal from 'sweetalert2';
+import { setTimeout } from 'timers';
 
 @Component({
   selector: 'app-tarea',
@@ -12,6 +17,11 @@ import { APIService } from 'src/app/services/api.service';
 export class TareaComponent implements OnInit {
 
   tareas: Tarea[] = [];
+  titulo: string = "";
+  descripcion: string = "";
+  tareaId: number = 0;
+  url: string = "";
+  @ViewChild('cerrar') btnCerrar!: ElementRef;
   constructor(private API: APIService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -19,11 +29,14 @@ export class TareaComponent implements OnInit {
     if (id == 0){
       let user: Usuario = JSON.parse(localStorage.getItem("usuario")||"") as Usuario;
       this.API.getEstudianteById(user.estudianteId || 0).subscribe( data => {
+        const entregasId = data.entregas.map(e => e.tareaId);
         data.estudiantesClases.forEach(ec => {
           ec.clase.tareas.forEach(t => {
-            let fecha = new Date(t.fechaEntrega)
-            t.fechaEntrega = `${fecha.getDate()}/${fecha.getMonth()+1}/${fecha.getFullYear()}`;
-            this.tareas.push(t);
+            if (!entregasId.includes(t.id)){
+              let fecha = new Date(t.fechaEntrega)
+              t.fechaEntrega = `${fecha.getDate()}/${fecha.getMonth()+1}/${fecha.getFullYear()}`;
+              this.tareas.push(t);
+            }
           })
         })
       });
@@ -47,4 +60,66 @@ export class TareaComponent implements OnInit {
     return new Date() > new Date(`${mes}/${dia}/${year}`);
   }
 
+  setModal(titulo: string, descripcion: string, tareaId: number){
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.tareaId = tareaId;
+  }
+
+  onFileSelected(event: any){
+    const file:File = event.target.files[0];
+    if (file){
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (event: any) => {
+        this.url = event.target.result;
+      }
+    }
+  }
+
+  onSubmit(){
+    if (this.url == ""){
+      Swal.fire(
+        "Alerta",
+        "Seleccione un archivo",
+        "warning"
+      )
+    } else {
+      let entrega = new Entrega();
+      let user: Usuario = JSON.parse(localStorage.getItem("usuario")||"") as Usuario;
+      let docu = `${user.estudiante?.nombres}_${user.estudiante?.apellidos}_${this.tareaId}`;
+      entrega.documento = docu;
+      entrega.tareaId = this.tareaId;
+      setDoc(doc(db, "entregas", docu), {
+        url: this.url
+      });
+      this.API.insertEntrega(entrega).subscribe(post => {
+        Swal.fire(
+          'Correcto',
+          'La tarea fue entregada correctamente',
+          'success'
+        )
+      }, error => {
+        Swal.fire(
+          'Error',
+          error.error,
+          'error'
+        )
+      })
+      this.tareas = [];
+      this.btnCerrar.nativeElement.click();
+      this.API.getEstudianteById(user.estudianteId || 0).subscribe( data => {
+        const entregasId = data.entregas.map(e => e.tareaId);
+        data.estudiantesClases.forEach(ec => {
+          ec.clase.tareas.forEach(t => {
+            if (!entregasId.includes(t.id)){
+              let fecha = new Date(t.fechaEntrega)
+              t.fechaEntrega = `${fecha.getDate()}/${fecha.getMonth()+1}/${fecha.getFullYear()}`;
+              this.tareas.push(t);
+            }
+          })
+        })
+      });
+    }
+  }
 }
